bfb8d1f09e484bd11b39b6c87c46cfa2df743690
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2022-11-27 19:39:49 +0100

    Update PrimaryInput.js

--- assets/components/inputs/PrimaryInput.js
+++ assets/components/inputs/PrimaryInput.js
@@ -1,25 +1,27 @@
 import { Pressable, TextInput, View, StyleSheet } from "react-native";
-import {Ionicons} from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
 
-const PrimaryInput = ({placeholder, externalStyle, catchInputData})=> {
-    const passInputData = (userInput)=>{
-        catchInputData(userInput)
-    }
+const PrimaryInput = (props) => {
+    const { placeholder } = props
     return (
-        <TextInput onChangeText={passInputData} placeholder={placeholder} style={[styles.inputStyle,externalStyle]}></TextInput>
+        <TextInput
+            placeholder={placeholder}
+            style={styles.inputStyle}
+            {...props}
+        />
     )
-} 
+}
 export default PrimaryInput;
 
 const styles = StyleSheet.create({
-    inputStyle :{
-        width:'90%',
-        height : 30,
-        color:'#01065B',
-        fontWeight:'700',
-        fontSize:14,
+    inputStyle: {
+        width: '90%',
+        height: 30,
+        color: '#01065B',
+        fontWeight: '700',
+        fontSize: 14,
         margin: 15,
         borderBottomWidth: 1,
-        borderBottomColor:'#01065B'
+        borderBottomColor: '#01065B'
     }
 })
